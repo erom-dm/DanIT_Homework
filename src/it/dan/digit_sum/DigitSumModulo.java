f2d4a2b6ca349816f6ae9c97dc367ee45ac3e700
erom-dm <32650878+erom-dm@users.noreply.github.com>
2018-04-29 19:57:22 +0300

    Digit sum using modulo

--- src/it/dan/digit_sum/DigitSumModulo.java
+++ src/it/dan/digit_sum/DigitSumModulo.java
@@ -1,4 +1,24 @@
+// Find the sum of digits within an integer
+
 package it.dan.digit_sum;
 
+import java.util.Scanner;
+
 public class DigitSumModulo {
+    public static void main(String[] args) {
+        System.out.print("Provide an integer: ");
+        Scanner in = new Scanner(System.in);
+        // User input
+        int num = in.nextInt();
+        int initialNum = num;
+        int sum = 0;
+
+        // Get right-most digit within the integer, then divide int by 10 without remainder.
+        while(num != 0){
+            sum += num % 10;
+            num /= 10;
+        }
+
+        System.out.println("The sum of digits within int: " + initialNum + " is " + sum);
+    }
 }
