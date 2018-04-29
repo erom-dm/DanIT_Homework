fc0c28bd90d7d7030d44f5ea4beaf8458b825fe2
erom-dm <32650878+erom-dm@users.noreply.github.com>
2018-04-29 20:37:48 +0300

    Digit sum with string

--- src/it/dan/digit_sum/DigitSumString.java
+++ src/it/dan/digit_sum/DigitSumString.java
@@ -1,4 +1,27 @@
+// Find the sum of digits within an integer
+
 package it.dan.digit_sum;
 
+import java.util.Scanner;
+
 public class DigitSumString {
+    public static void main(String[] args) {
+        System.out.print("Provide an integer: ");
+        Scanner in = new Scanner(System.in);
+        // User input
+        int num = in.nextInt();
+        int initialNum = num;
+        int sum = 0;
+
+        // Convert integer to char array. Get the sum of all numeric values within the array.
+        char[] arr = String.valueOf(num).toCharArray();
+        int len = arr.length;
+
+        for (int i = len-1; i >= 0; i--){
+            sum += Character.getNumericValue(arr[i]);
+        }
+
+        System.out.println("The sum of digits within int: " + initialNum + " is " + sum);
+
+    }
 }
