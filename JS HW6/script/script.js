6e1b5cf77aa618da26de55bba7147f4df98e32d4
erom-dm <32650878+erom-dm@users.noreply.github.com>
2018-09-02 18:08:57 +0300

    JS HW6 +setters - getters

--- JS HW6/script/script.js
+++ JS HW6/script/script.js
@@ -1,10 +1,36 @@
 function User(){
-    this.firstName = prompt("give me first name: ");
-    this.lastName = prompt("give me last name: ");
     this.getLogin = function () {
         return (this.firstName.slice(0,1) + this.lastName).toLowerCase();
-    }
+    };
+
+    this.setFirstName = function (name) {
+        Object.defineProperty(this, 'firstName', {
+            value: prompt("Give me first name: "),
+            writable: true
+        });
+
+        Object.defineProperty(this, 'firstName', {
+            writable: false
+        })
+    };
+
+    this.setLastName = function (name) {
+        Object.defineProperty(this, 'lastName', {
+            value: prompt("Give me first name: "),
+            writable: true
+        });
+
+        Object.defineProperty(this, 'lastName', {
+            writable: false
+        })
+    };
+
+
+
 }
+
 user1 = new User();
+user1.setFirstName();
+user1.setLastName();
 
 alert(user1.getLogin());
